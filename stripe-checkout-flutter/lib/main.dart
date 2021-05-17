e25ae7ccf2a489e8cc4458041a6185150f7955c6
Jerim Kaura <jerimkaura001@gmail.com>
2021-05-17 09:44:29 +0300

    stripe-checkout-flutter

---
+++ stripe-checkout-flutter/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe_payments/pages/cards.dart';
+import 'package:flutter_stripe_payments/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => HomePage(),
+        '/existing-cards': (context) => ExistingCardsPage()
+      },
+    );
+  }
+}
---
+++ stripe-checkout-flutter/lib/pages/cards.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+import 'package:flutter_stripe_payments/services/payment.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:stripe_payment/stripe_payment.dart';
+
+class ExistingCardsPage extends StatefulWidget {
+  ExistingCardsPage({Key key}) : super(key: key);
+
+  @override
+  ExistingCardsPageState createState() => ExistingCardsPageState();
+}
+
+class ExistingCardsPageState extends State<ExistingCardsPage> {
+  List cards = [
+    {
+      'cardNumber': '4242424242424242',
+      'expiryDate': '04/24',
+      'cardHolderName': 'John Doe',
+      'cvvCode': '424',
+      'showBackView': false,
+    },
+    {
+      'cardNumber': '5555555566554444',
+      'expiryDate': '04/23',
+      'cardHolderName': 'Jerim Kaura',
+      'cvvCode': '123',
+      'showBackView': false,
+    },
+    {
+      'cardNumber': '3755555566554444',
+      'expiryDate': '04/27',
+      'cardHolderName': 'Van Waler',
+      'cvvCode': '123',
+      'showBackView': false,
+    }
+  ];
+
+  payViaExistingCard(BuildContext context, card) async {
+    ProgressDialog dialog = new ProgressDialog(context);
+    dialog.style(message: 'Please wait...');
+    await dialog.show();
+    var expiryArr = card['expiryDate'].split('/');
+    CreditCard stripeCard = CreditCard(
+      number: card['cardNumber'],
+      expMonth: int.parse(expiryArr[0]),
+      expYear: int.parse(expiryArr[1]),
+    );
+    var response =
+        await StripeService.payViaExistingCard(amount: '2500', currency: 'USD', card: stripeCard);
+    await dialog.hide();
+    Scaffold.of(context)
+        .showSnackBar(SnackBar(
+          content: Text(response.message),
+          duration: new Duration(milliseconds: 1200),
+        ))
+        .closed
+        .then((_) {
+      Navigator.pop(context);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CHOOSE CARD'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: cards.length,
+          itemBuilder: (BuildContext context, int index) {
+            var card = cards[index];
+            return InkWell(
+              onTap: () {
+                payViaExistingCard(context, card);
+              },
+              child: CreditCardWidget(
+                cardNumber: card['cardNumber'],
+                expiryDate: card['expiryDate'],
+                cardHolderName: card['cardHolderName'],
+                cvvCode: card['cvvCode'],
+                showBackView: false,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
